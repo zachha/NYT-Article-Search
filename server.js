417426fbab7c33908b31cd6dc372b8790a9738fe
zachha <zachha@gmail.com>
2018-04-26 18:13:25 -0400

    server added

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const articleRoutes = require("./routes/articleApi");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// initialize body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// initializes mongoose for deployment or local
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreview");
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add API routes
+app.use("/api/article", articleRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
