6faec51e283fde8b54a9f8e896a2f3d929d31704
zachha <zachha@gmail.com>
2018-04-29 17:51:41 -0400

    mongodb uri added and server update

--- server.js
+++ server.js
@@ -24,11 +24,17 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// initializes mongoose for deployment or local
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
 
 
 app.listen(PORT, function() {
